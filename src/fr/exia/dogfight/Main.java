36d02c913b82da92762f1877ad79eb5f46df0115
Matiyeu <matthieu.crtd@gmail.com>
2019-05-03 14:14:43 +0200

    initial commit

---
+++ src/fr/exia/dogfight/Main.java
@@ -0,0 +1,14 @@
+package fr.exia.dogfight;
+
+/**
+ * Created by Matiyeu on 03/05/2019.
+ **/
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
